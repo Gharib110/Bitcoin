f0f175f7e936d2c8be2bda4af1d2c8b8f65e711e
Alireza Gharib <alirezagharib110@gmail.com>
2025-03-13 18:56:39 +0330

    Wrote tests for elliptic_curve pkg, mitigating the problems within the code and restructuring it a little

--- elliptic_curve/private-key.go
+++ elliptic_curve/private-key.go
@@ -2,26 +2,27 @@ package elliptic_curve
 
 import (
 	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"math/big"
 )
 
 type PrivateKey struct {
 	secret *big.Int
-	point  *Point // Public Key
+	point  *Point
 }
 
-func NewPrivateKey(sec *big.Int) *PrivateKey {
+func NewPrivateKey(secret *big.Int) *PrivateKey {
 	G := GetGenerator()
 	return &PrivateKey{
-		secret: sec,
-		// Public Key
-		point: G.ScalarMul(sec),
+		secret: secret,
+		//public key
+		point: G.ScalarMul(secret),
 	}
 }
 
 func (p *PrivateKey) String() string {
-	return fmt.Sprintf("private key hex : %x", p.secret)
+	return fmt.Sprintf("private key hex:{%s}", p.secret)
 }
 
 func (p *PrivateKey) GetPublicKey() *Point {
@@ -29,24 +30,34 @@ func (p *PrivateKey) GetPublicKey() *Point {
 }
 
 func (p *PrivateKey) Sign(z *big.Int) *Signature {
+	//(s, r)
+	//s = (z + r * e) / k
+	// k is a strong random number
 	n := GetBitCoinValueN()
 	k, err := rand.Int(rand.Reader, n)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Sign err with rand int: %s", err))
 	}
 	kField := NewFieldElement(n, k)
 	G := GetGenerator()
-
+	// s = (z + r * e) / k
+	// r = G * k
 	r := G.ScalarMul(k).x.num
 	rField := NewFieldElement(n, r)
 	eField := NewFieldElement(n, p.secret)
 	zField := NewFieldElement(n, z)
-
+	// r*e
 	rMulSecret := rField.Mul(eField)
-	zAddMulSecret := zField.Add(rMulSecret)
-	KInverse := kField.Inverse()
-	sField := zAddMulSecret.Mul(KInverse)
-
+	// z+r*e
+	zAddRMulSecret := zField.Add(rMulSecret)
+	// /k
+	kInverse := kField.Inverse()
+	sField := zAddRMulSecret.Mul(kInverse)
+	/*
+	   if s > n / 2 we need to change it to n - s, when doing signature
+	   verify, s and n - s are equivalence doing this change is for malleability reasons, detail:
+	   https://bitcoin.stackexchange.com/questions/85946/low-s-value-in-bitcoin-signature
+	*/
 	var opDiv big.Int
 	if sField.num.Cmp(opDiv.Div(n, big.NewInt(int64(2)))) > 0 {
 		var opSub big.Int
@@ -57,4 +68,44 @@ func (p *PrivateKey) Sign(z *big.Int) *Signature {
 		r: NewFieldElement(n, r),
 		s: sField,
 	}
+
+}
+
+/*
+Wif
+1, set first byte to 0x80 main-net, 0xef testnet
+2, append the bytes array of private key beside first byte,
+if the length of bytes array < 32 bytes, append leading 0 to 32 bytes
+3, if public SEC compressed, add suffix byte 0x01
+4, do hash256 on result of step 3 and get its first 4 bytes
+5, append the 4 bytes from step 4 with result from step 3 and do base58 encoding
+
+4,5 base58checksum
+*/
+func (p *PrivateKey) Wif(compressed bool, testnet bool) string {
+	var bytes []byte
+	if testnet {
+		bytes = append(bytes, 0xef)
+	} else {
+		bytes = append(bytes, 0x80)
+	}
+
+	secretBytes := p.secret.Bytes()
+	if len(secretBytes) < 32 {
+		//two chars turn into one byte
+		s := fmt.Sprintf("%064x", secretBytes)
+		paddingBytes, err := hex.DecodeString(s)
+		if err != nil {
+			panic(fmt.Sprintf("padding secret bytes err: %v\n", err))
+		}
+		secretBytes = paddingBytes
+	}
+
+	bytes = append(bytes, secretBytes...)
+	if compressed {
+		bytes = append(bytes, 0x01)
+	}
+
+	return Base58Checksum(bytes)
+
 }
--- elliptic_curve/util.go
+++ elliptic_curve/util.go
@@ -3,8 +3,9 @@ package elliptic_curve
 import (
 	"crypto/sha256"
 	"github.com/tsuna/endian"
-	"golang.org/x/crypto/ripemd160"
 	"math/big"
+
+	"golang.org/x/crypto/ripemd160"
 )
 
 type LittleEndianLength int
@@ -15,28 +16,6 @@ const (
 	LittleEndian8Bytes
 )
 
-func LittleEndianToBigInt(data []byte, length LittleEndianLength) *big.Int {
-	switch length {
-	case LittleEndian2Bytes:
-		p := new(big.Int)
-		p.SetBytes(data)
-		val := endian.NetToHostUint16(uint16(p.Uint64()))
-		return big.NewInt(int64(val))
-	case LittleEndian4Bytes:
-		p := new(big.Int)
-		p.SetBytes(data)
-		val := endian.NetToHostUint32(uint32(p.Uint64()))
-		return big.NewInt(int64(val))
-	case LittleEndian8Bytes:
-		p := new(big.Int)
-		p.SetBytes(data)
-		val := endian.NetToHostUint64(p.Uint64())
-		return big.NewInt(int64(val))
-	}
-
-	return nil
-}
-
 func BigIntToLittleEndian(v *big.Int, length LittleEndianLength) []byte {
 	switch length {
 	case LittleEndian2Bytes:
@@ -44,11 +23,13 @@ func BigIntToLittleEndian(v *big.Int, length LittleEndianLength) []byte {
 		littleEndianVal := endian.HostToNetUint16(uint16(val))
 		p := big.NewInt(int64(littleEndianVal))
 		return p.Bytes()
+
 	case LittleEndian4Bytes:
 		val := v.Int64()
 		littleEndianVal := endian.HostToNetUint32(uint32(val))
 		p := big.NewInt(int64(littleEndianVal))
 		return p.Bytes()
+
 	case LittleEndian8Bytes:
 		val := v.Int64()
 		littleEndianVal := endian.HostToNetUint64(uint64(val))
@@ -59,7 +40,34 @@ func BigIntToLittleEndian(v *big.Int, length LittleEndianLength) []byte {
 	return nil
 }
 
-// Hash256  z, sha256(sha256(create a text0) -> 256bits integer -> 32bytes integer
+func LittleEndianToBigInt(bytes []byte, length LittleEndianLength) *big.Int {
+	switch length {
+	case LittleEndian2Bytes:
+		p := new(big.Int)
+		p.SetBytes(bytes)
+		val := endian.NetToHostUint16(uint16(p.Uint64()))
+		return big.NewInt(int64(val))
+
+	case LittleEndian4Bytes:
+		p := new(big.Int)
+		p.SetBytes(bytes)
+		val := endian.NetToHostUint32(uint32(p.Uint64()))
+		return big.NewInt(int64(val))
+
+	case LittleEndian8Bytes:
+		p := new(big.Int)
+		p.SetBytes(bytes)
+		val := endian.NetToHostUint64(uint64(p.Uint64()))
+		return big.NewInt(int64(val))
+	}
+
+	return nil
+}
+
+/*
+z, sha256(sha256(create a text)) -> 256bits-> 32bytes integer
+*/
+
 func Hash256(text string) []byte {
 	hashOnce := sha256.Sum256([]byte(text))
 	hashTwice := sha256.Sum256(hashOnce[:])
@@ -68,44 +76,36 @@ func Hash256(text string) []byte {
 
 func GetGenerator() *Point {
 	Gx := new(big.Int)
+	Gx.SetString("79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 16)
 	Gy := new(big.Int)
-	Gx.SetString(
-		"79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
-		16)
-	Gy.SetString(
-		"483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8",
-		16)
+	Gy.SetString("483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16)
 	G := S256Point(Gx, Gy)
 	return G
 }
 
 func GetBitCoinValueN() *big.Int {
 	n := new(big.Int)
-	n.SetString(
-		"fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141",
-		16)
+	n.SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
 	return n
 }
 
-func ParseSECEncoding(secBin []byte) *Point {
-	//Check the first byte to decide it is compressed or uncompressed
+func ParseSEC(secBin []byte) *Point {
+	//check the first byte to decide it is compressed or uncompressed
 	if secBin[0] == 4 {
-		//uncompress
+		//uncompressed
 		x := new(big.Int)
 		x.SetBytes(secBin[1:33])
 		y := new(big.Int)
 		y.SetBytes(secBin[33:65])
-
 		return S256Point(x, y)
 	}
 
 	//check first byte for y is odd or even
 	isEven := secBin[0] == 2
 	x := new(big.Int)
-	x.SetBytes(secBin[1:33])
+	x.SetBytes(secBin[1:])
 	y2 := S256Field(x).Pow(big.NewInt(int64(3))).Add(S256Field(big.NewInt(int64(7))))
 	y := y2.Sqrt()
-
 	var modOp big.Int
 	var yEven *FieldElement
 	var yOdd *FieldElement
@@ -115,7 +115,6 @@ func ParseSECEncoding(secBin []byte) *Point {
 	} else {
 		yOdd = y
 		yEven = y.Negate()
-
 	}
 
 	if isEven {
@@ -125,9 +124,12 @@ func ParseSECEncoding(secBin []byte) *Point {
 	}
 }
 
-// EncodeBase58 which removes 0 O, l I
+/*
+EncodeBase58
+base58 it removes 0 O, l I
+*/
 func EncodeBase58(s []byte) string {
-	base58Alphabets := "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+	Base58Alphabet := "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 	count := 0
 	for idx := range s {
 		if s[idx] == 0 {
@@ -141,30 +143,29 @@ func EncodeBase58(s []byte) string {
 	for i := 0; i < count; i++ {
 		prefix += "1"
 	}
-
 	result := ""
 	num := new(big.Int)
 	num.SetBytes(s)
-	for num.Cmp(big.NewInt(0)) > 0 {
+	for num.Cmp(big.NewInt(int64(0))) > 0 {
 		var divOp big.Int
 		var modOp big.Int
 		mod := modOp.Mod(num, big.NewInt(int64(58)))
 		num = divOp.Div(num, big.NewInt(int64(58)))
-		result = string(base58Alphabets[mod.Int64()]) + result
+		result = string(Base58Alphabet[mod.Int64()]) + result
 	}
 
 	return prefix + result
 }
 
 func Base58Checksum(s []byte) string {
-	hash256 := Hash256(EncodeBase58(s))
+	hash256 := Hash256(string(s))
 	return EncodeBase58(append(s, hash256[:4]...))
 }
 
 func Hash160(s []byte) []byte {
-	sha256C := sha256.Sum256(s)
+	sum256 := sha256.Sum256(s)
 	hasher := ripemd160.New()
-	hasher.Write(sha256C[:])
+	hasher.Write(sum256[:])
 	hashBytes := hasher.Sum(nil)
 
 	return hashBytes
